13549f3e8a525216847927cef30d17f807c490f2
shadowshadow725 <shadowshadow725@gmail.com>
2022-03-27 17:13:45 -0400

    parse json into xs ys format

--- DataCollection/CheckDataDistribution.py
+++ DataCollection/CheckDataDistribution.py
@@ -6,10 +6,33 @@ import matplotlib.pyplot as plt
 
 
 def average_mmr(d: Dict) -> int:
-    for j in range(10):
+    s, d = 0, 0
 
-        if d['elo']:
-            return sum(d['elo'])/len(d['elo'])
+    for j in d['elo']:
+
+        if j > 0:
+            s += j
+            d += 1
+    return s/d
+
+
+def read_data_to_xs_ys():
+    matches = os.listdir(os.getcwd()+'\\data\\')
+    ys = []
+    xs = []
+
+    for i in matches:
+        try:
+            f = open('data\\' + i)
+            data = json.load(f)
+            if data['timeline']:
+                if data['elo']:
+                    xs.append(data['timeline'])
+                    ys.append(average_mmr(data))
+        except:
+            print(i, 'failed')
+
+    return xs, ys
 
 
 if __name__ == "__main__":
